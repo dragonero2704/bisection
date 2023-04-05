ea434297e7cf32ea4f016cb28f8dd0d135dda529
dragonero2704 <roberto.rudi04@gmail.com>
2023-04-05 09:36:14 +0200

    sium

--- bisection.py
+++ bisection.py
@@ -1,4 +1,21 @@
 from math import *
+from argparse import ArgumentParser, RawTextHelpFormatter
+version  = "0.0.1"
+title = f"""
+ /$$$$$$$  /$$                               /$$                                    
+| $$__  $$|__/                              |__/                                    
+| $$  \ $$ /$$  /$$$$$$$  /$$$$$$  /$$$$$$$$ /$$  /$$$$$$  /$$$$$$$   /$$$$$$       
+| $$$$$$$ | $$ /$$_____/ /$$__  $$|____ /$$/| $$ /$$__  $$| $$__  $$ /$$__  $$      
+| $$__  $$| $$|  $$$$$$ | $$$$$$$$   /$$$$/ | $$| $$  \ $$| $$  \ $$| $$$$$$$$      
+| $$  \ $$| $$ \____  $$| $$_____/  /$$__/  | $$| $$  | $$| $$  | $$| $$_____/      
+| $$$$$$$/| $$ /$$$$$$$/|  $$$$$$$ /$$$$$$$$| $$|  $$$$$$/| $$  | $$|  $$$$$$$      
+|_______/ |__/|_______/  \_______/|________/|__/ \______/ |__/  |__/ \_______/      
+                                                                                    
+                                                                                    
+author: dragonero2704
+version:{version}
+"""
+
 def bisection(a, b, g, iterations=1000, precision=None):
     for _ in range(iterations):
         Ya, Yb = g(a), g(b)
@@ -15,18 +32,35 @@ def bisection(a, b, g, iterations=1000, precision=None):
     return Xmedian
 
 def main():
-    expression = input("f(x): ")
+    parser = ArgumentParser(prog=__file__.split('\\')[-1],
+        usage="./%(prog)s",
+        formatter_class=RawTextHelpFormatter,
+        description=print(title),)
+    
+    parser.add_argument('-f','--function', dest="f", action="store", metavar="<Espressione matematica di f(x)>", help="-f x^3+x^2-4")
+    parser.add_argument('-a', dest='a', action="store", metavar="<Estremo inferiore [a,b]>", help="Estremo inferiore nell'intervallo [a,b]")
+    parser.add_argument('-b', dest='b', action="store", metavar="<Estremo superiore [a,b]>", help="Estremo superiore nell'intervallo [a,b]")
+    parser.add_argument('-i', '--iterations', dest='iterations', action="store", metavar="<numero di iterazioni>", help="Numero massimo di iterazioni della bisezione", default=1000)
+    parser.add_argument('-p', '--precision', dest='precision', action="store", metavar="<precisione di abs(a-b)/2>",help="Minimo intervallo [a,b]", default=None)
+    args = parser.parse_args()
+    # print(args)
+    f,a,b,i,p = args.f,args.a,args.b,args.iterations,args.precision
+
+    if f is None:
+        expression = input("f(x): ")
     expression = expression.replace('^','**')
     expression = expression.replace('[','(')
     expression = expression.replace(']',')')
     expression = expression.replace('{','(')
     expression = expression.replace('}',')')
-
     f = eval(f"lambda x:{expression}")
-    a = float(input("a: "))
-    b = float(input("b: "))
 
-    x = bisection(a,b,f)
+    if a is None:
+        a = float(input("a: "))
+    if b is None:
+        b = float(input("b: "))
+
+    x = bisection(a,b,f,i,p)
     print(x)
     pass
 
